d69220e8ba1155f2070a469ba93dabb639976c75
TharawitJ <thara10.tharawit@gmail.com>
2026-03-11 15:12:19 +0700

    prisma schema

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express from 'express'
 import authRouter from "./routes/auth.route.js"
 import postRouter from "./routes/post.route.js"
-// import errorMiddleWare from "/middlewares/error.middleware.js"
-
+import {errorMiddleWare} from "./middlewares/error.middleware.js"
+import {notFoundError} from "./middlewares/notFound.middleware.js"
 
 const app = express()
 app.use(express.json())
@@ -11,5 +11,8 @@ app.use("/auth",authRouter)
 app.use("/post",postRouter)
 
 
-// app.use(errorMiddleWare)
+app.use(errorMiddleWare)
+
+app.use(notFoundError)
+
 export default app
